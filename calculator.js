4e2e3806db32b401fcb39d872483c7476021e0d9
Meghana1991 <31301862+Meghana1991@users.noreply.github.com>
2025-03-27 23:43:39 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 class Calculator {
-    add(a, b) {
-        return a + b
+    add(...numbers) {
+        if(numbers.length === 0) throw new Error("No Numbers")        
+        return numbers.reduce((sum,num) => sum + num, 0)
     }
     mul(a, b) {
         return a * b
